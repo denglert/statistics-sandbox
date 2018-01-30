4e03e85545f171ed05ccd6d58d51abd35c15a938
denglert <englert.david@gmail.com>
2018-01-30 20:47:35 +0000

    Wilks theomrem unfinished project.

--- Wilks_theorem/stattools.py
+++ Wilks_theorem/stattools.py
@@ -2,32 +2,56 @@
 
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
+from scipy.optimize import minimize
 import numpy as np
 
-def NLL(pars, x):
-    mult = 2.0
-    ll = mult*(-0.5 * np.log(2.0*np.pi) - np.log(pars[1]) - (x-pars[0])**2/(2.0*pars[1]**2))
-    return -ll
 
-def NLL_comb(pars, X, NLL):
+### --- Negative log-likelihoods (NLL)
+
+def NLL_norm(pars, x):
+    """Returns -2*ln*L"""
+    mu = pars[0]
+    sigma = pars[1]
+    ll = -0.5 * np.log(2.0*np.pi) - np.log(sigma) - (x-mu)**2/(2.0*sigma**2)
+    return - 2.0 * ll
+
+
+def NLL(pars, X, NLL_func):
     nll_sum = 0
     for xi in X:
-        nll = NLL(pars, xi)
+        nll = NLL_func(pars, xi)
         nll_sum += nll
     return nll_sum
 
 
-def NLL_comb_conditional(par_opt, par_fix, x, NLL):
+def NLL_cond(par_opt, par_fix, X, NLL_func):
     pars = (par_opt, par_fix)
-    nll_sum = NLL_comb(pars, x, NLL)
+    nll_sum = NLL(pars, X, NLL_func)
     return nll_sum
 
 
+### --- Maximum likelihood estimators (MLEs)
+
+def MLE(NLL, X, NLL_func, init_values, method='Nelder-Mead'):
+    args = (X, NLL_func)
+    m = minimize(fun=NLL, x0=init_values, args=args, method=method)
+    return m
+
+
+def MLE_cond(NLL_cond, X, NLL_func, par_fix, init_values, method='Nelder-Mead'):
+    args = (par_fix, X, NLL_func)
+    m = minimize(fun=NLL_cond, x0=init_values, args=args, method=method)
+    return m
+
+
+### --- Plotting
+
 def plot_NLL(X,Y,Z):
+
     f,a = plt.subplots()
     
     pcm = a.pcolor(X, Y, Z, cmap='viridis_r', norm=colors.LogNorm(vmin=Z.min(), vmax=Z.max()))
-    cb = f.colorbar(pcm, ax=a)
+    cb = f.colorbar(pcm, ax=a, extend='max')
     cb.set_label(r'$-2\ln \mathcal{L}$')
         
     min_index = np.argmin(Z)
@@ -39,8 +63,3 @@ def plot_NLL(X,Y,Z):
     label = "Min value: {:.2e}".format(min_value)
     a.text(0.2, 0.1, label, transform=a.transAxes)
     return f,a
-
-
-def MLE(NLL_comb, x0, args, method='Nelder-Mead'):
-    m = minimize(fun=NLL_comb, x0=x0, args=args, method=method)
-    return m
